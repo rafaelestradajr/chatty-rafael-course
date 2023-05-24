050475f8c987e115d28b2096dc4d3548c770b6dd
Rafael Estrada <rafaelestrada@comcast.net>
2023-05-23 21:33:03 -0700

    redirect to chat page if logged in

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 import {useUser} from '@auth0/nextjs-auth0/client';
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/dist/server/api-utils";
 
 export default function Home() {
   const {isLoading, error,user} = useUser();
@@ -28,3 +30,18 @@ export default function Home() {
     </>
   );
 }
+
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession (ctx.req,ctx.res);
+  if (!!session){
+    return{
+    redirect:{
+      destination: '/chat',
+    },
+  };
+  }
+  return {
+   props:{},
+  };
+};
