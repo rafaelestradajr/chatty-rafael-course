fb4f5439b0166a3e5602140a3e8ea2df2f7a9496
Rafael Estrada <rafaelestrada@comcast.net>
2023-05-24 23:05:24 -0700

    create ChatSidebar component and footer

--- pages/chat/[[...chatid]].js
+++ pages/chat/[[...chatid]].js
@@ -2,11 +2,19 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>New Chat</title>
       </Head>
-      <h1>Welcome to the Chat id page</h1>
-    </div>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+      <div>sidebar</div>
+      <div className="bg-gray-700 flex flex-col">
+        
+        <div className=' flex-1'>chat window</div>
+      <footer className="bg-gray-800 p-10">footer</footer>
+      </div>
+
+      </div>
+    </>
   );
 }
